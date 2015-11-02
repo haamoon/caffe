e449aa372bdfbf6529d6df534e76b0e340c4520c
haamoon <amirreza_shaban@yahoo.com>
2015-11-02 14:55:04 -0500

    update

--- src/caffe/util/math_functions.cu
+++ src/caffe/util/math_functions.cu
@@ -292,7 +292,7 @@ __global__ void add_scalar_kernel(const int n, const Dtype alpha, Dtype* y) {
 }
 
 template <typename Dtype>
-__global__ void caffe_strided_add_scalar(const int n, const Dtype alpha, int incx, Dtype* y) {
+__global__ void caffe_strided_add_scalar_kernel(const int n, const Dtype alpha, int incx, Dtype* y) {
   CUDA_KERNEL_LOOP(index, n/incx) {
     y[index * incx] += alpha;
   }
@@ -300,16 +300,16 @@ __global__ void caffe_strided_add_scalar(const int n, const Dtype alpha, int inc
 
 
 template <>
-void caffe_gpu_strided_add_scalar(const int N, const float alpha, int incx, float *X) {
+void caffe_gpu_strided_add_scalar<float>(const int N, const float alpha, int incx, float *X) {
   // NOLINT_NEXT_LINE(whitespace/operators)
-  caffe_strided_add_scalar<float><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(
+  caffe_strided_add_scalar_kernel<float><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(
       N, alpha, incx, X);
 }
 
 template <>
-void caffe_gpu_strided_add_scalar(const int N, const double alpha, int incx, double *X) {
+void caffe_gpu_strided_add_scalar<double>(const int N, const double alpha, int incx, double *X) {
   // NOLINT_NEXT_LINE(whitespace/operators)
-  caffe_strided_add_scalar<double><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(
+  caffe_strided_add_scalar_kernel<double><<<CAFFE_GET_BLOCKS(N), CAFFE_CUDA_NUM_THREADS>>>(
       N, alpha, incx, X);
 }
 
