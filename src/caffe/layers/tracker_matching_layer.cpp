9865a7ff4a411fd9e8e22d6638cca2e1c59ef356
haamoon <amirreza_shaban@yahoo.com>
2015-11-21 19:03:43 -0500

    update

--- src/caffe/layers/tracker_matching_layer.cpp
+++ src/caffe/layers/tracker_matching_layer.cpp
@@ -20,7 +20,7 @@ template <typename Dtype>
 void TrackerMatchingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   CHECK_EQ(3, bottom[0]->num_axes()) << "Input(0) segment ranks should have 3 axis, "
-      << "corresponding to (num, segment_id, track_id)";     
+      << "corresponding to (num, track_id, segment_id)";
   CHECK_EQ(3, bottom[1]->num_axes()) << "Input(1) segment overlaps should have 3 axis, "
       << "corresponding to (num, segment_id, segment_id)";
       
@@ -28,14 +28,13 @@ void TrackerMatchingLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       << "corresponding to (num)";
   
   N_ = bottom[0]->shape(0);
-  max_nseg_ = bottom[0]->shape(1);
-  max_ntrack_ = bottom[0]->shape(2);
+  max_nseg_ = bottom[0]->shape(2);
+  max_ntrack_ = bottom[0]->shape(1);
     
   CHECK_EQ(N_, bottom[1]->shape(0));
   CHECK_EQ(N_, bottom[2]->shape(0));
   
   CHECK_EQ(max_nseg_, bottom[1]->shape(0));
-  CHECK_EQ(max_nseg_, bottom[1]->shape(1));
   CHECK_EQ(max_nseg_, bottom[1]->shape(1));
   
   
@@ -57,37 +56,38 @@ void TrackerMatchingLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom
   
   int* max_indeces = max_indeces_.mutable_cpu_data();
   
-  for(int i = 0; i < max_ntrack_; ++i) {
+  for(int track = 0; track < max_ntrack_; ++track) {
   	max_indeces[i] = 0;
   }
   
   for(int n = 0; n < N_; ++n) {
-  	for(int i = 1; i < seg_num[n]; ++i) {
-  		for(int j = 0; j < max_ntrack_; ++j) {
-  			if(v_data[max_indeces[j] * max_ntrack_ + j]	< 
-  				v_data[i * max_ntrack_ + j]) {
-  				max_indeces[j] = j;	
-  			}  		
+    for(int track = 0; track < max_ntrack_; ++track) {
+        for(int seg = 1; seg < seg_num[n]; ++seg) {
+            if(v_data[*max_indeces] < v_data[seg]) {
+                *max_indeces = seg;
+  		    }  		
+  		    max_indeces += 1;
+  		    v_data += max_nseg_;
   		}
   	}
-  }
-  
-
-  for(int n = 0; n < N_; ++n) {
-  	for(int i = 1; i < seg_num[n]; ++i) {
-  		for(int j = 0; j < max_ntrack_; ++j) {
-  			top_data[i * max_ntrack_ + j] = 
-  					overlaps_data[i * max_ntrack_ + max_indeces[j]]; 
-  		}
-  	}
+  	max_indeces -= max_ntrack_;
+  	for(int track = 0; track < max_ntrack_; ++track) {
+        for(int seg = 1; seg < max_nseg; ++seg) {
+            top_data[seg] = overlaps_data[*max_indeces * max_nseg_ + seg];    
+        }
+        *max_indeces = 0;
+        max_indeces += 1;
+        top_data += max_nseg_;
+    }
+    max_indeces -= max_ntrack_;
   }
 }
 
-//#ifdef CPU_ONLY
-//STUB_GPU(TrackerMatchingLayer);
-//#endif
+#ifdef CPU_ONLY
+STUB_GPU(TrackerMatchingLayer);
+#endif
 
 INSTANTIATE_CLASS(TrackerMatchingLayer);
-REGISTER_LAYER_CLASS(SuperpixelPooling);
+REGISTER_LAYER_CLASS(TrackerMatching);
 
 }  // namespace caffe
